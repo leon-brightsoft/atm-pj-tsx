1a3dd171c570358c4a316425c50fe5ccefb93179
Leon <leon.brightsoft@gmail.com>
2021-12-01 15:24:08 +0700

    fix add queue

--- FE/src/services/queue.service.ts
+++ FE/src/services/queue.service.ts
@@ -4,7 +4,6 @@ import { Queue } from "../interfaces/atm.interface";
 const BASE_URL = "http://localhost:5000/api/v1/atms/";
 
 export const addQueueApi = async (data: Queue) => {
-    const queue = {}
     return await axios.post(BASE_URL + "people", data);
 }
 
