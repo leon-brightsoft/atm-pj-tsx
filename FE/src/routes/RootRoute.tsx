a3dd905a5056a2e8a92f23b837b6c58199cc9c06
Leon <leon.brightsoft@gmail.com>
2021-12-01 13:32:22 +0700

    fix lost token when f5

--- FE/src/routes/RootRoute.tsx
+++ FE/src/routes/RootRoute.tsx
@@ -1,16 +1,23 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "../screens/Login";
 import Register from "../screens/Register";
 import Home from "../screens/home/Home";
 import AuthRequired from "./AuthRequired";
+import { useSelector } from "react-redux";
+import { accessToken } from "../services/token.sevice";
 
 const RootRoute: React.FC = () => {
+  const token = useSelector((state: any) => state.auth.token);
+  useEffect(() => {
+    accessToken(token);
+  }, [token]);
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/" element={<Navigate to="/home"/>}/>
+      <Route path="/" element={<Navigate to="/home" />} />
       <Route
         path="/home"
         element={
